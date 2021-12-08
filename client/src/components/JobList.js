ba27653b2c460cbbe2a2c704d6c4b7119928a4e3
María Mora Bernabé <maria.moraber@gmail.com>
2021-12-08 13:34:04 +0100

    admin dashboard offers

--- client/src/components/JobList.js
+++ client/src/components/JobList.js
@@ -5,24 +5,24 @@ function JobList(props) {
     return (
         <div className="JobList">
             <div className="container">
-            <div className="row">
-                {props.jobs.map((job) => (
-                <div key={job.id} className="col-lg-4 col-md-6">
-                    <div className="card shadow mb-3 rounded-border">
-                    <div className="card-header">{job.title}
-                    <div className="card-body">
-                        <h3 className="card-title">{job.title}</h3>
-                        <h6 className="card-text">{job.company}</h6>
-                        <p className="card-text">Salary: {job.salary}</p>
-                        <a href="#" className="btn btn-primary">
-                        Apply!
-                        </a>
-                    </div>
-                    </div>
+                <div className="row">
+                    {props.jobs && props.jobs.map((job) => (
+                        <div key={job.id} className="col-lg-4 col-md-6">
+                            <div className="card shadow mb-3 rounded-border">
+                                <div className="card-header">{job.title}
+                                    <div className="card-body">
+                                        <h3 className="card-title">{job.title}</h3>
+                                        <h6 className="card-text">{job.company}</h6>
+                                        <p className="card-text">Salary: {job.salary}</p>
+                                        <a href="#" className="btn btn-primary">
+                                            Apply!
+                                        </a>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                    ))}
                 </div>
-                </div>
-                ))}
-            </div>
             </div>
         </div>
     );
